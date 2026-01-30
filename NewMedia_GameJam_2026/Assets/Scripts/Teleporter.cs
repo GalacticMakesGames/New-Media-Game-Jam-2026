c17edf560a0a13661ef971113db18e18a32ad35d
ZINBOYZ <nathanlops1228@gmail.com>
2026-01-29 23:52:35 -0500

    updated camera zoom to follow size update for teleporters

--- NewMedia_GameJam_2026/Assets/Scripts/Teleporter.cs
+++ NewMedia_GameJam_2026/Assets/Scripts/Teleporter.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Teleporter : MonoBehaviour
 {
     // Reference to the destination transform
     public Transform destination;
     [SerializeField] AudioClip teleport;
+    public float bearScale = .01f;
+    public Transform targetTransform;
+    public GameObject targetObject;
+    public CinemachineVirtualCamera vcam;
+    public float cineZoom = .01f;
 
+    private void Start()
+    {
+
+        targetObject = GameObject.Find("Player");
+        if (targetObject != null)
+        {
+            targetTransform = targetObject.transform;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -16,6 +31,11 @@ public class Teleporter : MonoBehaviour
             other.transform.position = destination.position;
 
             AudioSource.PlayClipAtPoint(teleport, transform.position);
+
+            targetTransform.localScale = new Vector3(bearScale, bearScale, bearScale);
+
+            vcam.m_Lens.OrthographicSize = cineZoom;
+
         }
     }
 }
